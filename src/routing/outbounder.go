841b8edb43a4e72cbb10d0ba9ac8db3f61cc67d8
John Bass <John_Bass2@cable.comcast.com>
2017-02-09 20:03:45 -0800

    Skeleton inbounder; simplified the outbounder

--- src/routing/outbounder.go
+++ src/routing/outbounder.go
@@ -16,6 +16,9 @@ const (
 	// OutbounderKey is the Viper subkey which is expected to hold Outbounder configuration
 	OutbounderKey = "device.outbound"
 
+	// OutboundContentType is the Content-Type header value for device messages leaving talaria
+	OutboundContentType = "application/wrp"
+
 	EventPrefix = "event:"
 	URLPrefix   = "url:"
 
@@ -23,7 +26,6 @@ const (
 	DefaultEventEndpoint                     = "http://localhost:8090/api/v2/notify"
 	DefaultAssumeScheme                      = "https"
 	DefaultAllowedScheme                     = "https"
-	DefaultContentType                       = "application/wrp"
 	DefaultTimeout             time.Duration = 10 * time.Second
 	DefaultMaxIdleConns                      = 0
 	DefaultMaxIdleConnsPerHost               = 100
@@ -42,18 +44,30 @@ type Outbounder struct {
 	DeviceNameHeader    string
 	AssumeScheme        string
 	AllowedSchemes      []string
-	ContentType         string
 	Timeout             time.Duration
 	MaxIdleConns        int
 	MaxIdleConnsPerHost int
 	IdleConnTimeout     time.Duration
+	Logger              logging.Logger
 }
 
 // NewOutbounder returns an Outbounder unmarshalled from a Viper environment.
 // This function allows the Viper instance to be nil, in which case a default
 // Outbounder is returned.
-func NewOutbounder(v *viper.Viper) (o *Outbounder, err error) {
-	o = new(Outbounder)
+func NewOutbounder(logger logging.Logger, v *viper.Viper) (o *Outbounder, err error) {
+	o = &Outbounder{
+		Method:              DefaultMethod,
+		EventEndpoint:       DefaultEventEndpoint,
+		DeviceNameHeader:    device.DefaultDeviceNameHeader,
+		AssumeScheme:        DefaultAssumeScheme,
+		AllowedSchemes:      []string{DefaultAllowedScheme},
+		Timeout:             DefaultTimeout,
+		MaxIdleConns:        DefaultMaxIdleConns,
+		MaxIdleConnsPerHost: DefaultMaxIdleConnsPerHost,
+		IdleConnTimeout:     DefaultIdleConnTimeout,
+		Logger:              logger,
+	}
+
 	if v != nil {
 		err = v.Unmarshal(o)
 	}
@@ -61,133 +75,42 @@ func NewOutbounder(v *viper.Viper) (o *Outbounder, err error) {
 	return
 }
 
-func (o *Outbounder) method() string {
-	if len(o.Method) > 0 {
-		return o.Method
-	}
-
-	return DefaultMethod
-}
-
-func (o *Outbounder) eventEndpoint() string {
-	if len(o.EventEndpoint) > 0 {
-		return o.EventEndpoint
-	}
-
-	return DefaultEventEndpoint
-}
-
-func (o *Outbounder) deviceNameHeader() string {
-	if len(o.DeviceNameHeader) > 0 {
-		return o.DeviceNameHeader
-	}
-
-	return device.DefaultDeviceNameHeader
-}
-
-func (o *Outbounder) assumeScheme() string {
-	if len(o.AssumeScheme) > 0 {
-		return o.AssumeScheme
-	}
-
-	return DefaultAssumeScheme
-}
-
-func (o *Outbounder) allowedSchemes() map[string]bool {
-	if len(o.AllowedSchemes) > 0 {
-		schemes := make(map[string]bool, len(o.AllowedSchemes))
-		for _, scheme := range o.AllowedSchemes {
-			schemes[scheme] = true
-		}
-
-		return schemes
-	}
-
-	return map[string]bool{
-		DefaultAllowedScheme: true,
-	}
-}
-
-func (o *Outbounder) contentType() string {
-	if len(o.ContentType) > 0 {
-		return o.ContentType
-	}
-
-	return DefaultContentType
-}
-
-func (o *Outbounder) timeout() time.Duration {
-	if o.Timeout > 0 {
-		return o.Timeout
-	}
-
-	return DefaultTimeout
-}
-
-func (o *Outbounder) maxIdleConns() int {
-	if o.MaxIdleConns > 0 {
-		return o.MaxIdleConns
-	}
-
-	return DefaultMaxIdleConns
-}
-
-func (o *Outbounder) maxIdleConnsPerHost() int {
-	if o.MaxIdleConnsPerHost > 0 {
-		return o.MaxIdleConnsPerHost
-	}
-
-	return DefaultMaxIdleConnsPerHost
-}
-
-func (o *Outbounder) idleConnTimeout() time.Duration {
-	if o.IdleConnTimeout > 0 {
-		return o.IdleConnTimeout
-	}
-
-	return DefaultIdleConnTimeout
-}
-
-func (o *Outbounder) newTransport() *http.Transport {
+func (o *Outbounder) NewTransport() *http.Transport {
 	return &http.Transport{
-		MaxIdleConns:        o.maxIdleConns(),
-		MaxIdleConnsPerHost: o.maxIdleConnsPerHost(),
-		IdleConnTimeout:     o.idleConnTimeout(),
+		MaxIdleConns:        o.MaxIdleConns,
+		MaxIdleConnsPerHost: o.MaxIdleConnsPerHost,
+		IdleConnTimeout:     o.IdleConnTimeout,
 	}
 }
 
-func (o *Outbounder) newTransactor() func(*http.Request) (*http.Response, error) {
+func (o *Outbounder) NewTransactor() func(*http.Request) (*http.Response, error) {
 	client := &http.Client{
-		Transport: o.newTransport(),
-		Timeout:   o.timeout(),
+		Transport: o.NewTransport(),
+		Timeout:   o.Timeout,
 	}
 
 	return client.Do
 }
 
-// newRequestFactory produces a RequestFactory function that creates an outbound HTTP request
+// NewRequestFactory produces a RequestFactory function that creates an outbound HTTP request
 // for a given WRP message from a specific device.  Once created, the returned factory is isolated
 // from any changes made to this Outbounder instance.
-func (o *Outbounder) newRequestFactory() RequestFactory {
-	var (
-		method           = o.method()
-		eventEndpoint    = o.eventEndpoint()
-		contentType      = o.contentType()
-		deviceNameHeader = o.deviceNameHeader()
-		assumeScheme     = o.assumeScheme()
-		allowedSchemes   = o.allowedSchemes()
-	)
+func (o *Outbounder) NewRequestFactory() RequestFactory {
+	allowedSchemes := make(map[string]bool, len(o.AllowedSchemes))
+	for _, scheme := range o.AllowedSchemes {
+		allowedSchemes[scheme] = true
+	}
 
 	return func(d device.Interface, raw []byte, message *wrp.Message) (r *http.Request, err error) {
 		if strings.HasPrefix(message.Destination, EventPrefix) {
 			// route this to the configured endpoint that receives all events
-			r, err = http.NewRequest(method, eventEndpoint, bytes.NewBuffer(raw))
+			r, err = http.NewRequest(o.Method, o.EventEndpoint, bytes.NewBuffer(raw))
 		} else if strings.HasPrefix(message.Destination, URLPrefix) {
 			// route this to the given URL, subject to some validation
-			if r, err = http.NewRequest(method, message.Destination[len(URLPrefix):], bytes.NewBuffer(raw)); err == nil {
+			if r, err = http.NewRequest(o.Method, message.Destination[len(URLPrefix):], bytes.NewBuffer(raw)); err == nil {
 				if len(r.URL.Scheme) == 0 {
 					// if no scheme is supplied, use the configured AssumeScheme
-					r.URL.Scheme = assumeScheme
+					r.URL.Scheme = o.AssumeScheme
 				} else if !allowedSchemes[r.URL.Scheme] {
 					err = fmt.Errorf("Scheme not allowed: %s", r.URL.Scheme)
 				}
@@ -200,8 +123,8 @@ func (o *Outbounder) newRequestFactory() RequestFactory {
 			return
 		}
 
-		r.Header.Set(deviceNameHeader, string(d.ID()))
-		r.Header.Set("Content-Type", contentType)
+		r.Header.Set(o.DeviceNameHeader, string(d.ID()))
+		r.Header.Set("Content-Type", OutboundContentType)
 		// TODO: Need to set Convey?
 
 		return
@@ -210,29 +133,29 @@ func (o *Outbounder) newRequestFactory() RequestFactory {
 
 // NewMessageListener returns a MessageListener which dispatches an HTTP transaction
 // for each WRP message.
-func (o *Outbounder) NewMessageListener(logger logging.Logger) device.MessageListener {
+func (o *Outbounder) NewMessageListener() device.MessageListener {
 	var (
-		transactor     = o.newTransactor()
-		requestFactory = o.newRequestFactory()
+		transactor     = o.NewTransactor()
+		requestFactory = o.NewRequestFactory()
 	)
 
 	return func(d device.Interface, raw []byte, message *wrp.Message) {
 		request, err := requestFactory(d, raw, message)
 		if err != nil {
-			logger.Error("Unable to create request for device [%s]: %s", d.ID(), err)
+			o.Logger.Error("Unable to create request for device [%s]: %s", d.ID(), err)
 			return
 		}
 
 		response, err := transactor(request)
 		if err != nil {
-			logger.Error("HTTP error for device [%s]: %s", d.ID(), err)
+			o.Logger.Error("HTTP error for device [%s]: %s", d.ID(), err)
 			return
 		}
 
 		if response.StatusCode < 400 {
-			logger.Debug("HTTP response for device [%s]: %s", d.ID(), response.Status)
+			o.Logger.Debug("HTTP response for device [%s]: %s", d.ID(), response.Status)
 		} else {
-			logger.Error("HTTP response for device [%s]: %s", d.ID(), response.Status)
+			o.Logger.Error("HTTP response for device [%s]: %s", d.ID(), response.Status)
 		}
 	}
 }
