2405334a66d1d678fec4d15c545fdcfb88b3360f
nharte968 <nicholas_harter2@cable.comcast.com>
2017-12-04 16:55:28 -0800

    device connection handler with Authorization header option

--- src/talaria/primaryHandler.go
+++ src/talaria/primaryHandler.go
@@ -23,9 +23,11 @@ import (
 	"github.com/Comcast/webpa-common/device"
 	"github.com/Comcast/webpa-common/secure"
 	"github.com/Comcast/webpa-common/secure/handler"
+	"github.com/Comcast/webpa-common/secure/key"
 	"github.com/Comcast/webpa-common/wrp"
 	"github.com/go-kit/kit/log"
 	"github.com/gorilla/mux"
+	"github.com/SermoDigital/jose/jwt"
 	"github.com/spf13/viper"
 )
 
@@ -37,20 +39,66 @@ const (
 
 	// TODO: This should be configurable at some point
 	poolSize = 1000
+	
+	DefaultKeyId = "current"
 )
+
+type JWTValidator struct {
+	// JWTKeys is used to create the key.Resolver for JWT verification keys
+	Keys key.ResolverFactory `json:"keys"`
+
+	// Custom is an optional configuration section that defines
+	// custom rules for validation over and above the standard RFC rules.
+	Custom secure.JWTValidatorFactory `json:"custom"`
+}
 
 func NewPrimaryHandler(logger log.Logger, manager device.Manager, v *viper.Viper) (http.Handler, error) {
 	var (
-		authKey                = v.GetString("inbound.authKey")
-		r                      = mux.NewRouter()
-		apiHandler             = r.PathPrefix(fmt.Sprintf("%s/%s", baseURI, version)).Subrouter()
-		authorizationDecorator = func(h http.Handler) http.Handler { return h }
+		authKey                      = v.GetString("inbound.authKey")
+		r                            = mux.NewRouter()
+		apiHandler                   = r.PathPrefix(fmt.Sprintf("%s/%s", baseURI, version)).Subrouter()
+		authorizationDecorator       = func(h http.Handler) http.Handler { return h }
+		authorizationDecoratorDevice = func(h http.Handler) http.Handler { return h }
 	)
 
 	if len(authKey) > 0 {
 		authorizationDecorator = handler.AuthorizationHandler{
 			Logger:    logger,
 			Validator: secure.ExactMatchValidator(authKey),
+		}.Decorate
+	}
+	
+	if v.IsSet("jwtValidtors") {
+		var validator secure.Validator
+		var cfg_validators []JWTValidator
+
+		if err := v.UnmarshalKey("jwtValidators", &cfg_validators); err != nil {
+			validator = make(secure.Validators, 0, 0)
+		} else {
+			validators := make(secure.Validators, 0, len(cfg_validators))
+			
+			for _, validatorDescriptor := range cfg_validators {
+				keyResolver, err := validatorDescriptor.Keys.NewResolver()
+				if err != nil {
+					return nil, err
+				}
+
+				validators = append(
+					validators,
+					secure.JWSValidator{
+						DefaultKeyId:  DefaultKeyId,  // todo: where is this defined?
+						Resolver:      keyResolver,
+						JWTValidators: []*jwt.Validator{validatorDescriptor.Custom.New()},
+					},
+				)
+			}
+
+			validator = validators
+		}
+
+		authorizationDecoratorDevice = handler.AuthorizationHandler{
+			Logger:    logger,
+			Validator: validator,
 		}.Decorate
 	}
 
@@ -75,6 +123,17 @@ func NewPrimaryHandler(logger log.Logger, manager device.Manager, v *viper.Viper
 		Registry: manager,
 	})).
 		Methods("GET")
+
+	// the connect handler decorated for authorization
+	apiHandler.Handle(
+		"/device",
+		authorizationDecoratorDevice(
+			device.UseID.FromHeader(&device.ConnectHandler{
+				Logger:    logger,
+				Connector: manager,
+			}),
+		),
+	).HeadersRegexp("Authorization", ".*")
 
 	// the connect handler is not decorated for authorization
 	apiHandler.Handle(
