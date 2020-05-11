b0b509917e5bbb94b6793b386cdc8b7b8b2b8dc5
Joel Unzain <joel_unzain@comcast.com>
2020-05-11 16:20:06 -0700

    properly convert an int to a string

--- deviceStatus.go
+++ deviceStatus.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/xmidt-org/webpa-common/convey"
@@ -21,7 +22,7 @@ func statusMetadata(d device.Interface) map[string]string {
 		wrpmeta.Field{From: "fw-name", To: "/fw-name"},
 		wrpmeta.Field{From: "last-reconnect-reason", To: "/last-reconnect-reason"},
 		wrpmeta.Field{From: "protocol", To: "/protocol"}).
-		Set("/trust", string(d.Metadata().JWTClaims().Trust())).
+		Set("/trust", strconv.Itoa(d.Metadata().JWTClaims().Trust())).
 		Build()
 
 	if allFieldsPresent {
