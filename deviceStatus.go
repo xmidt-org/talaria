6182921051cc49df0f1d29193e096a5db8d8b505
ChaitanyaSingla <charlieksingla@gmail.com>
2024-12-13 16:51:37 +0530

    Add hgw-mac key in metadata while preparing online and offline message

--- deviceStatus.go
+++ deviceStatus.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strconv"
 	"time"
+	"strings"
 
 	"github.com/xmidt-org/webpa-common/v2/convey"
 	"github.com/xmidt-org/webpa-common/v2/device"
@@ -36,6 +37,7 @@ func statusMetadata(d device.Interface) map[string]string {
 		metadata["/compliance"] = convey.MissingFields.String()
 	}
 
+	metadata["hw-mac"] = strings.TrimPrefix(string(d.ID()), "mac:")
 	return metadata
 }
 
